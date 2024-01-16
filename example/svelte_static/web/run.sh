4c28a74c092d239a9fd5a7f90da3ea16fe38955d
Jacob Phillips <jifalops@gmail.com>
2024-01-16 08:55:37 -0500

    state before backing out of attempt at getting sverdle to work automatically. it works when running run.sh manually, but when launched automatically in vscode (postStartCommand, tasks.json) the site launches but sverdle doesnt work. docker compose up does work though. opting to change to a simple setup becuase it seems an upstream issue

--- example/svelte_static/web/run.sh
+++ example/svelte_static/web/run.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 set -e
-npm install > /dev/null
-npm run dev -- --host 0.0.0.0 > ../local/npm_run_dev.log 2>&1 &
-tail -f ../local/npm_run_dev.log
+cd "$(dirname "$0")"
+if ps -aux | grep '[n]pm run dev'; then
+    echo "Server is already running."
+    tail -f ../local/npm_run_dev.log
+else
+    npm install > /dev/null
+    npm run dev -- --host 0.0.0.0 2>&1 | tee ../local/npm_run_dev.log
+fi
