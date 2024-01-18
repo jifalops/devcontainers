df24a0efc237057ae00a0c83c32cd71f0c2dfb03
Jacob Phillips <jifalops@gmail.com>
2024-01-18 08:53:10 -0500

    ~/.venv is more standard than ~/.local/venv

--- scripts/setup-python.sh
+++ scripts/setup-python.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-if [ ! -d "$HOME/.local/venv" ]; then
-    python3 -m venv "$HOME/.local/venv"
-    echo 'PATH="$HOME/.local/venv/bin:$PATH"' >> "$HOME/.bashrc"
-    source "$HOME/.local/venv/bin/activate"
+if [ ! -d "$HOME/.venv" ]; then
+    python3 -m venv "$HOME/.venv"
+    PATH="$HOME/.venv/bin:$PATH"
+    echo 'PATH="$HOME/.venv/bin:$PATH"' >> "$HOME/.bashrc"
 fi
