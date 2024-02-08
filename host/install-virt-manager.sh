aa33f504c4bb33d838813b7a8450c0211a09aa40
Jacob Phillips <jifalops@gmail.com>
2024-02-08 16:18:47 -0500

    add virt manager xml for win11

--- host/install-virt-manager.sh
+++ host/install-virt-manager.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 set -e
-sudo apt install -y --no-install-recommends qemu-kvm libvirt-clients libvirt-daemon-system bridge-utils virtinst libvirt-daemon virt-manager qemu-utils dnsmasq virt-viewer
+sudo apt install -y --no-install-recommends qemu-kvm libvirt-clients libvirt-daemon-system bridge-utils virtinst libvirt-daemon gir1.2-spiceclientgtk-3.0 ovmf swtpm swtpm-tools qemu-utils dnsmasq virt-manager virt-viewer
 # Fix installation issues on chromeos
-printf 'user = "root"\ngroup = "root"\nremember_owner = 0\ncgroup_device_acl=[]\n' | sudo tee -a /etc/libvirt/qemu.conf
+printf 'user = "remember_owner = 0\nnamespaces=[]\n' | sudo tee -a /etc/libvirt/qemu.conf
 
 # Add groups
 sudo usermod -aG libvirt "$USER"
-sudo usermod -aG libvirt-qemu "$USER"
 sudo usermod -aG kvm "$USER"
+
 # Simulate a login
 sudo su "$USER"
+
+# Use this guide to setup Windows 11 on a chromebook:
+# https://www.reddit.com/r/ChromeOSFlex/comments/ucno4b/comment/i6cviv8
